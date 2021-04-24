5ab2a15887b666fca1b12ee02a417434ae0365c8
Rodrigo Fontes <fontes.rodrigo14@gmail.com>
2021-04-23 21:25:01 -0300

    Initial commit

---
+++ Assets/Scripts/AIControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    //crinado a variavel NavMeshAgent
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        //Ao iniciar o script, pega o componente NavMeshAgent do Gameobject
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+}
---
+++ Assets/Scripts/AgentManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    GameObject[] agents;
+    void Start()
+    {
+        //Pegando gameobjcts que possuem ai com tag
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+    void Update()
+    {
+        //ao clicar com botao do mouse 0 entra no if
+        if (Input.GetMouseButtonDown(0))
+        {
+            //criando um raycasthit
+            RaycastHit hit;
+            
+            //criar uma linha reta para verificar se o player clicou em algum local
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                foreach(GameObject a in agents)
+                {
+                    //setando o destino dos Navmesh aonde o player clicou na tela
+                    a.GetComponent<AIControl>().agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
